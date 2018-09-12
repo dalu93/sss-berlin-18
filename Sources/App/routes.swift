8e9193b7707d4e304d4948e5d3dadb1614b88bca
D'Alberti, Luca <luca.dalberti@merck.com>
2018-09-12 10:38:10 +0200

    first commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -7,9 +7,27 @@ public func routes(_ router: Router) throws {
         return "Hello, world!"
     }
 
+    struct JSONExample: Content {
+        let name: String
+        let age: Int
+        let birthday: Date
+    }
+
+    router.get("json") { _ in
+        return JSONExample(
+            name: "Hello",
+            age: 1,
+            birthday: Date()
+        )
+    }
+
     // Example of configuring a controller
     let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+    let todos = router.grouped("todos")
+    todos.get(use: todoController.index)
+    todos.get(Todo.parameter, use: todoController.view)
+    todos.post(use: todoController.create)
+    todos.patch(Todo.parameter, use: todoController.update)
+    todos.delete(Todo.parameter, use: todoController.delete)
+    todos.delete(use: todoController.clear)
 }
